a98a3cd96a51d2b207f6087fffe123f1b8a1ae76
pranay banait <151549022+pranay1222@users.noreply.github.com>
2026-03-25 13:21:44 +0530

    Initialize Express server with welcome route

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Node-app!' });
+});
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
